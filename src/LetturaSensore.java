5ca52cba77aeb5a193312009f18d5f5322dbb0c1
davide-bise <12999297@itisrossi.vi.it>
2026-09-25 11:56:45 +0200

    finished ex1

--- src/LetturaSensore.java
+++ src/LetturaSensore.java
@@ -1,5 +1,7 @@
 import java.util.Optional;
 
+import static java.lang.IO.println;
+
 class LetturaSensore{
     private Double temperatura;
     private Integer umiditaPercentuale;
@@ -18,14 +20,80 @@ class LetturaSensore{
         this.timestampUnix = timestampUnix;
         this.batteriaScarica = batteriaScarica;
     }
+    public Double getTemperatura() {
+        return temperatura;
+    }
+    public Integer getUmiditaPercentuale() {
+        return umiditaPercentuale;
+    }
+    public Long getTimestampUnix() {
+        return timestampUnix;
+    }
+    public Boolean getBatteriaScarica() {
+        return batteriaScarica;
+    }
     public static Optional<LetturaSensore> parsePacchetto(String dato){
-        Double temperatura = 0.0;
-        Integer umiditaPercentuale = 0;
-        Long timestampUnix = 0L;
-        Boolean batteriaScarica = false;
-
-        LetturaSensore sensore= new LetturaSensore(null,umiditaPercentuale,timestampUnix
+        Double temperatura;
+        Integer umiditaPercentuale;
+        Long timestampUnix;
+        Boolean batteriaScarica;
+        try {
+            temperatura = Double.parseDouble(dato.split(";")[0].split("=")[1]);
+        }
+        catch(NumberFormatException ex){
+            println("campo temperatura corrotto!");
+            temperatura = null;
+        }
+        catch(IndexOutOfBoundsException ex){
+            println("campo temperatura vuoto!");
+            temperatura = null;
+        }
+        try {
+            umiditaPercentuale = Integer.parseInt(dato.split(";")[1].split("=")[1]);
+            if(umiditaPercentuale < 0 || umiditaPercentuale > 100){
+                throw new IllegalArgumentException();
+            }
+        }
+        catch(NumberFormatException ex){
+            println("campo umidita corrotto!");
+            umiditaPercentuale = null;
+        }
+        catch(IndexOutOfBoundsException ex){
+            println("campo umidita vuoto!");
+            umiditaPercentuale = null;
+        }
+        catch(IllegalArgumentException ex){
+            println("campo umidita fuori range!");
+            umiditaPercentuale = null;
+        }
+        try {
+            timestampUnix = Long.parseLong(dato.split(";")[2].split("=")[1]);
+        }
+        catch(NumberFormatException ex){
+            println("campo timestamp corrotto!");
+            timestampUnix = null;
+        }
+        catch(IndexOutOfBoundsException ex){
+            println("campo timestamp vuoto!");
+            timestampUnix = null;
+        }
+        try {
+            batteriaScarica = Boolean.parseBoolean(dato.split(";")[3].split("=")[1]);
+        }
+        catch(NumberFormatException ex){
+            println("campo batteria corrotto!");
+            batteriaScarica = null;
+        }
+        catch(IndexOutOfBoundsException ex){
+            println("campo batteria vuoto!");
+            batteriaScarica = null;
+        }
+        LetturaSensore sensore= new LetturaSensore(temperatura,umiditaPercentuale,timestampUnix
                 ,batteriaScarica);
-        return Optional.of(sensore);
+        return Optional.of(sensore);  //necessario se si vuole restituire l'optional
+    }
+    @Override
+    public String toString(){
+        return "Temperatura:" + temperatura + "Umidita:" + umiditaPercentuale + "timestamp:" + timestampUnix + "batteria:" + batteriaScarica;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,43 @@
+import static java.lang.IO.println;
 
 void main(){
-    Optional<LetturaSensore> sensore = LetturaSensore.parsePacchetto("ciao");
+    println("Lettura 1:");
+    Optional<LetturaSensore> sensore = LetturaSensore.parsePacchetto("temp=23.5;umid=61;ts=1732000000;batt_low=false");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 2:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=xx.xx;umid=14;ts=2742455610;batt_low=true");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 3:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=14.6;umid=ciao;ts=6936001200;batt_low=false");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 4:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=;umid=150;ts=gg;batt_low=false");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 5:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=30.3;umid=34;ts=1732000000;batt_low=");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 6:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=27.8;umid=76;ts=1732000000;batt_low=true");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 7:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=12.9;umid=;ts=1732000000;batt_low=false");
+    println("\n"+sensore.toString()+"\n\n");
+    println("Lettura 8:");
+    sensore=null;
+    sensore = LetturaSensore.parsePacchetto("temp=21.2;umid=45;ts=nooo;batt_low=false");
+    println("\n"+sensore.toString()+"\n\n");
+    Integer valore1 = 62;
+    Integer  valore2 = 67;
+    ConfrontaBatteria(valore1,valore2);
+}
+void ConfrontaBatteria(Integer valore1, Integer valore2){
+    println("Confronto valori:"+valore1+" - "+valore2);
+    println(valore1.equals(valore2));
+    return;
 }
